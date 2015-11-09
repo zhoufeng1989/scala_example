9bbe33f2ff286a590cdd6fdf7a8cb82522794f0b
zhoufeng <zhoufengloop@gmail.com>
2015-11-09 22:25:31 +0800

    Add examples.

--- src/main/scala/exercises/Generator.scala
+++ src/main/scala/exercises/Generator.scala
@@ -1,8 +1,56 @@
 package exercises
 
+import java.util.Random
+
 /**
  * Created by zhoufeng on 15/11/9.
  */
-trait Generator {
+trait Generator[+T] {
+  self =>
 
+  def generate: T
+
+  def map[S](f: T => S): Generator[S] = new Generator[S] {
+    override def generate: S = f(self.generate)
+  }
+
+  def flatMap[S](f: T => Generator[S]): Generator[S] = new Generator[S] {
+    override def generate: S = f(self.generate).generate
+  }
+}
+
+object Generator {
+  def integers = new Generator[Int] {
+    val rand = new Random
+    def generate = rand.nextInt
+  }
+
+  val booleans = integers map (_ > 0)
+
+  def doubles = new Generator[Double] {
+    val rand = new Random
+    def generate = rand.nextDouble
+  }
+
+  def pairs[T, U](t: Generator[T], u: Generator[U]): Generator[(T, U)] = for (x <- t; y <- u) yield (x, y)
+
+  def single[T](t: T): Generator[T] = new Generator[T] {
+    def generate = t
+  }
+
+  def choose(lo: Int, hi: Int): Generator[Int] = for (x <- integers) yield lo + scala.math.abs(x) % (hi - lo)
+
+  def oneOf[T](xs: T*): Generator[T] = for(x <- choose(0, xs.length)) yield xs(x)
+
+  def emptyLists = single(Nil)
+
+  def noEmptyLists = for{
+    head <- integers;
+    tail <- lists
+  }yield head::tail
+
+  def lists = for {
+    x <- booleans
+    list <- if (x) emptyLists else noEmptyLists
+  }yield list
 }
--- src/main/scala/exercises/JSON.scala
+++ src/main/scala/exercises/JSON.scala
@@ -3,6 +3,24 @@ package exercises
 /**
  * Created by zhoufeng on 15/11/9.
  */
-trait JSON {
+abstract class JSON
+case class JSeq(elems: List[JSON]) extends JSON
+case class JObj(bindings: Map[String, JSON]) extends JSON
+case class JNum(num: Double) extends JSON
+case class JStr(str: String) extends JSON
+case class JBool(b: Boolean) extends JSON
+case object JNull  extends JSON
 
+object JSON {
+  def show(json: JSON): String = json match {
+    case JNum(num) => num.toString
+    case JStr(str) => "\"" + str + "\""
+    case JBool(b) => b.toString
+    case JNull => "Null"
+    case JSeq(elems) => "[" + elems.map(show).mkString(", ") + "]"
+    case JObj(bindings) => {
+      val assocs = bindings.map{case (key, value) => "\"" + key + "\":" + show(value)}
+      "{" + assocs.mkString(",")  + "}"
+    }
+  }
 }
